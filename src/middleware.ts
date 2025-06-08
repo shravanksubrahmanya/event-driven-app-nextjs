46d575d4f23b0055a6cff2a75e20436e73b9d4d9
Shravan K Subrahmanya <shravanksubrahmanya@gmail.com>
2025-06-08 11:06:25 +0530

    Worked on middlewares to properly handle the authentication and authorizations

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,62 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import {
+  clerkClient,
+  clerkMiddleware,
+  createRouteMatcher,
+} from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export default clerkMiddleware();
+// Define public routes
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/api/webhook/register",
+  "/sign-up(.*)",
+  "/sign-in(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req: NextRequest) => {
+  const { userId, redirectToSignIn } = await auth();
+
+  if (isPublicRoute(req)) {
+    return NextResponse.next();
+  }
+
+  if (!userId) {
+    return redirectToSignIn({ returnBackUrl: req.url });
+  }
+
+  if (userId) {
+    try {
+      const clerk = await clerkClient();
+      const user = await clerk.users.getUser(userId);
+      const role = (user.publicMetadata?.role as string) || undefined;
+
+      if (role === "admin" && req.nextUrl.pathname.startsWith("/dashboard")) {
+        return NextResponse.redirect(new URL("/admin/dashboard", req.url));
+      }
+
+      // prevent non admin users from accessing admin routes
+      if (role !== "admin" && req.nextUrl.pathname.startsWith("/admin")) {
+        return NextResponse.redirect(new URL("/dashboard", req.url));
+      }
+
+      // redirect auth user trying to access public routes
+      if (isPublicRoute(req)) {
+        return NextResponse.redirect(
+          new URL(role === "admin" ? "/admin/dashboard" : "/dashboard", req.url)
+        );
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+      return NextResponse.redirect(new URL("/error", req.url));
+    }
+  }
+
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
